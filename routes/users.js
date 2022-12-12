234c51cfdc2e3af1433413eacafa330f74fe9e59
Sai Tejas A R <saitejaspallavi@gmail.com>
2022-12-12 22:01:31 +0530

    Update user succesfully done

--- routes/users.js
+++ routes/users.js
@@ -13,6 +13,14 @@ router.put("/:id", async(req,res)=>{
                 return res.status(500).json(err)
             }
         }
+        try{
+            const user = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            });
+            res.status(200).json("Account has been updated")
+        }catch(err){
+            return res.status(500).json(err);
+        }
 
     } else{
         return res.status(403).json("You can update only your account!");
