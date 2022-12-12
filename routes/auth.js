4ed942d29b35851a8c558f97f71c0dd45ee6d796
Sai Tejas A R <saitejaspallavi@gmail.com>
2022-12-12 20:21:31 +0530

    Succesfully encrypted pass

--- routes/auth.js
+++ routes/auth.js
@@ -10,13 +10,15 @@ router.post("/register", async (req,res)=>{
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
+
+        //create new user
         const  newUser = new User({
             username:req.body.username,
             email:req.body.email,
             password:hashedPassword,
         });
 
-
+        //save user and respond
         const user = await newUser.save();
         res.status(200).json(user);
 
