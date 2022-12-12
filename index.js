9cb34fe10fe3c52fa9c47e4572ec901f2c167aae
Sai Tejas A R <saitejaspallavi@gmail.com>
2022-12-12 17:23:38 +0530

    Added Env Variables, Setup DB, Getting Started

--- index.js
+++ index.js
@@ -7,6 +7,24 @@ const morgan = require("morgan");
 
 dotenv.config();
 
+mongoose.connect(
+    process.env.MONGO_URL, 
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+    console.log("Connected to MongoDB")
+}
+);
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req,res)=>{
+    res.send("Welcome to Homepage")
+})
+
+
 
 app.listen(8800,()=>{
     console.log("Backend Server is running!")
