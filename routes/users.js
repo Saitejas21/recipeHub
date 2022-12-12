174f57d08c24de8930dee928a321a6f9ffcf7b08
Sai Tejas A R <saitejaspallavi@gmail.com>
2022-12-12 21:48:41 +0530

    login done, letting users update their details

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,26 @@
+const User = require("../models/User")
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 
-router.get("/",(req,res)=>{
-    res.send("hey its user route")
+//update user
+router.put("/:id", async(req,res)=>{
+    if(req.body.userId === req.params.id || req.user.isAdmin){
+        if(req.body.password){
+            try{
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            }catch(err){
+                return res.status(500).json(err)
+            }
+        }
+
+    } else{
+        return res.status(403).json("You can update only your account!");
+    }
 })
+//delete user
+//get a user
+//follow a user 
+//unfollow a user
 
 module.exports = router
